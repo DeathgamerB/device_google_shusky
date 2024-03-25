98988b8952f064327c72d268d3a1728cc9213266
DeathgamerB <87858321+DeathgamerB@users.noreply.github.com>
2024-03-25 15:52:46 +0100

    update files

--- custom_shiba.mk
+++ custom_shiba.mk
@@ -4,27 +4,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Evolution X stuff.
-TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 TARGET_IS_PIXEL := true
 TARGET_IS_PIXEL_8 := true
 TARGET_PIXEL_STAND_SUPPORTED := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Inherit device configuration
 $(call inherit-product, device/google/shusky/aosp_shiba.mk)
-$(call inherit-product, device/google/zuma/evolution_common.mk)
+$(call inherit-product, device/google/zuma/custom_common.mk)
 
-include device/google/shusky/shiba/device-evolution.mk
+include device/google/shusky/shiba/device-custom.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 8
-PRODUCT_NAME := evolution_shiba
+PRODUCT_NAME := custom_shiba
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_PRODUCT=shiba \
